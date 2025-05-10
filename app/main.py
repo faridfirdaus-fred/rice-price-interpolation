17fd9c0d2a5c2c01d74ad1dd78f5cffadc811bff
faridfred <147456315+faridfirdaus-fred@users.noreply.github.com>
2025-05-10 10:20:16 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,8 +9,10 @@ from pydantic import BaseModel
 import json
 import uvicorn
 
+import uvicorn
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000)
+
     
 load_dotenv()
 
